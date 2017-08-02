7487a634b0f3308e8a42a73648fc5c56710eeaa3
Antonio Ricardo <antonio.eschola@gmail.com>
2017-08-02 12:17:18 -0400

    First commit

---
+++ accounts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.core.urlresolvers import reverse
+from django.views.generic import CreateView
+
+# Classe de registro de usuário
