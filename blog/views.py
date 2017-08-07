7b038aa6cdf698b906c1fb7eb7d66661c02da424
Antonio Ricardo <antonio.eschola@gmail.com>
2017-08-07 07:40:12 -0400

    Adding editing blog to home page and more features

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.core.urlresolvers import reverse
 from django.http.response import HttpResponseRedirect
 from django.utils.text import slugify
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
+from django.views.generic import TemplateView
 
 # importando o form para criação do Blog
 from blog.forms import BlogForm
@@ -34,3 +35,29 @@ class NewBlogView(CreateView):
             return HttpResponseForbidden('Você não pode criar mais de um blog')
         else:
             return super(NewBlogView, self).dispatch(request, *args, **kwargs)
+
+# definindo a home da app
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(HomeView, self).get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated():
+            if Blog.objects.filter(owner=self.request.user).exists():
+                ctx['has_blog'] = True
+                ctx['blog'] = Blog.objects.filter(owner=self.request.user).last()
+        return ctx
+
+
+
+# definindo a view de edição do blog
+class UpdateBlogView(UpdateView):
+    form_class = BlogForm
+    template_name = 'blog_settings.html'
+    success_url = '/'
+    model = Blog
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(UpdateBlogView, self).dispatch(request, *args, **kwargs)
--- blueblog/urls.py
+++ blueblog/urls.py
@@ -3,14 +3,16 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from accounts.views import UserRegistrationView
 from django.contrib.auth.views import login, logout
-# importando view para criação do blog
-from blog.views import NewBlogView
+# importando view para criação, home e edição do blog
+from blog.views import NewBlogView, HomeView, UpdateBlogView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     url(r'^new-user/$', UserRegistrationView.as_view(), name='user_registration'),
     url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', logout, {'next_page': '/login/'}, name='logout'),
     url(r'^blog/new/$', NewBlogView.as_view(), name='new-blog'),
+    url(r'^blog/(?P<pk>\d+)/update/$', UpdateBlogView.as_view(), name='update-blog'),
+
 ]
